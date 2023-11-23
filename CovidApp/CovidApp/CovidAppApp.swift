15a78f5a2754d9dfb1162d37a996429bcc797a06
XxGonanxX <patomorelia@hotmail.com>
2023-11-23 11:20:21 -0600

    feature: first commit

---
+++ CovidApp/CovidApp/CovidAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidAppApp.swift
+//  CovidApp
+//
+//  Created by Alan Patricio González Bernal on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CovidAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
